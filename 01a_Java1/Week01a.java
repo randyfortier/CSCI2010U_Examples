5ae5df60b8f0630a8eca0ff2ff0863df2b9d0576
randyfortier <randy.fortier@gmail.com>
2019-09-11 11:00:08 -0400

    Week 01b example

--- 01a_Java1/Week01a.java
+++ 01a_Java1/Week01a.java
@@ -26,8 +26,75 @@ public class Week01a {
          System.out.println("Age: " + Week01a.age);
 
          Week01a w1 = new Week01a();
-         System.out.println("Age: " + w1.age);
+         // Can't access non-static here in main():
+         //System.out.println("Age: " + w1.age);
          System.out.println("Name: " + w1.name);
 
+         long bankBalance = 100000000000L;
+         Integer otherAccount = 500;
+         
+         float gpa = 2.4f;
+         String ageStr = "20 years old";
+         try {
+            age = Integer.parseInt(ageStr);
+            // succeeded
+         } catch (NumberFormatException e) {
+            // failed
+            e.printStackTrace();
+         } finally {
+            // succeeded or failed
+         }
+         System.out.printf("Your age is %d.\n", age);
+
+         char keyPressed = 'B';
+         String name = "Randy";
+         if ((keyPressed == 'A') && (name.equals("Randy"))) {
+            System.out.println("Welcome to the CSIS!");
+         } else if (name.equals("Justin")) {
+            System.out.println("Welcome PM!");
+         } else {
+            System.out.println("No entry!");
+         }
+
+         int choice = 7;
+         switch (choice) {
+             case 0:
+                // do something for 0
+                break;
+             case 1:
+                // do something for 1
+                break;
+            default:
+                // all other cases
+
+         }
+
+         int hp = -30;
+         hp = (hp < 0) ? 0 : hp;
+
+         int numTurns = 3;
+         while (numTurns > 0) {
+             // do something
+             System.out.println("Hello from while");
+             numTurns--;
+         }
+
+         numTurns = 3;
+         do {
+             // do something
+             System.out.println("Hello from do-while");
+             numTurns--;
+         } while (numTurns > 0);
+
+         numTurns = 3;
+         for (int i = numTurns; i > 0; i--) {
+             // do something
+             System.out.println("Hello from for");
+         }
+
+         float[] grades = new float[] {27.0f, 30.0f, 15.0f, 19.0f};
+         for (float grade : grades) {
+            System.out.println("Grade: " + grade);
+         }
      }
 }

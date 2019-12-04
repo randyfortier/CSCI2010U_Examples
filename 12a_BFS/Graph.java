6964ee4908c34721339da871e402c2b82c6c4d0b
randyfortier <randy.fortier@gmail.com>
2019-12-04 12:29:17 -0500

    Added BFS demo

--- 12a_BFS/Graph.java
+++ 12a_BFS/Graph.java
@@ -5,7 +5,8 @@ public class Graph {
    private class Vertex {
       private String name;
       private List<Vertex> adjacencies;
-      // add extra fields
+      private boolean discovered = false;
+      private Vertex parent = null;
 
       public Vertex(String name) {
          this.name = name;
@@ -24,7 +25,11 @@ public class Graph {
          return adjacencies.size();
       }
 
-      // add accessors for new fields
+      public boolean isDiscovered() { return this.discovered; }
+      public void setDiscovered(boolean newValue) { this.discovered = newValue; }
+
+      public Vertex getParent() { return parent; }
+      public void setParent(Vertex newParent) { this.parent = newParent; }
    }
 
    private HashMap<String, Vertex> vertices;
@@ -51,7 +56,35 @@ public class Graph {
       return vertices.get(name);
    }
 
-   // BFS
+   public Vertex bfs(String startingVertexName, String goalVertexName) throws InterruptedException {
+      Vertex startingVertex = vertices.get(startingVertexName);
+      startingVertex.setParent(null);
+      startingVertex.setDiscovered(true);
+
+      LinkedBlockingQueue<Vertex> queue = new LinkedBlockingQueue<Vertex>();
+      queue.put(startingVertex);
+
+      while (queue.size() > 0) {
+         Vertex v = queue.remove();
+
+         // check if we have found the goal state
+         if (v.getName().equals(goalVertexName)) {
+            return v;
+         }
+
+         // search the adjacencies for v
+         List<Vertex> adjacencies = v.getAdjacencies();
+         for (Vertex w: adjacencies) {
+            if (!w.isDiscovered()) {
+               w.setDiscovered(true);
+               w.setParent(v);
+               queue.put(w);
+            }
+         }
+      }
+
+      return null;
+   }
 
    public static void main(String[] args) {
       Graph graph = new Graph();
@@ -69,6 +102,34 @@ public class Graph {
       graph.addEdge("B", "E");
       graph.addEdge("D", "E");
 
+      Vertex d = graph.getVertex("D");
+      System.out.println("degree of D: " + d.getDegree());
+      List<Vertex> adjacencies = d.getAdjacencies();
+      System.out.println("Adjacencies of D: ");
+      for (Vertex v: adjacencies) {
+         System.out.print("\t" + v.getName() + ", adjacencies: ");
+
+         for (Vertex a: v.getAdjacencies()) {
+            System.out.print("\t\t" + a.getName() + " ");
+         }
+         System.out.println("");
+      }
+
       // test BFS
+      try {
+         System.out.println("Running BFS:");
+         Vertex current = graph.bfs("E", "A");
+         if (current == null) {
+            System.out.println("No path found from E to A");
+         } else {
+
+            while (current != null) {
+               System.out.print(current.getName() + " <- ");
+               current = current.parent;
+            }
+         }
+      } catch (InterruptedException e) {
+         e.printStackTrace();
+      }
    }
 }
